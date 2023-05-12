b2cadc67688d716b3083a08daf9657da566909a0
Aiden B <133374942+VeganMessiah@users.noreply.github.com>
2023-05-12 16:05:39 +0100

    Add files via upload

---
+++ Icecream shop.py
@@ -0,0 +1,22 @@
+print("Welcome! We have the following flavours:")
+print("Strawberry, Vanilla or Mint")
+fFlav = input("Please enter your first flavour:")
+print(fFlav)
+sFlav = input("Please enter your second flavour:")
+print(sFlav)
+print("Here is your yummy",fFlav,"and",sFlav,"ice cream!")
+if fFlav =="Vanilla":
+    print("*As you selected Vanilla, you get a free wafer!")
+if sFlav =="Vanilla":
+    print("*As you selected Vanilla, you get a free wafer!")
+if sFlav == "Mint":
+    print("*Your second flavour was Mint, so here is an extra scoop of",fFlav)
+if fFlav =="Strawberry":
+    print("*You made a really tasty choice!")
+
+
+
+
+
+
+
